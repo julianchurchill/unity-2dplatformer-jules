b4b74c0164567d058a77c672bfc4aad75dc8d074
Julian Churchill <chewielouie03@gmail.com>
2020-04-25 16:38:10 +0100

    Added coin script, score manager and UI display for score

---
+++ Assets/Coin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public int coinValue = 1;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.instance.ChangeScore(coinValue);
+        }
+    }
+}
