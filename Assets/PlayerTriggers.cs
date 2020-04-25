c597eacafb117040d2b79b22c1ae5b6d4220fada
Julian Churchill <chewielouie03@gmail.com>
2020-04-25 15:30:12 +0100

    Added destruction of coins on collision with player

---
+++ Assets/PlayerTriggers.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerTriggers : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("coin"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
